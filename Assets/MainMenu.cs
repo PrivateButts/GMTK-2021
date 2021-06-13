35b58fae24bd3727e52a85029caaa09b0cf8753e
Mephflynn <Kennedyalex117@yahoo.com>
2021-06-13 12:54:36 -0400

    menu finished

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
-   public void StartGame()
+    public GameObject mainMenu, optionsMenu, creditsMenu;
+    public GameObject menuFirstButton, optionsFirstButton, optionsClosedButton, creditsFirstButton, creditsBackButton;
+    
+
+    public void OpenOptions()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        optionsMenu.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+
+        EventSystem.current.SetSelectedGameObject(optionsFirstButton);
     }
 
-    public void QuitGame()
+    public void CloseOptions()
     {
-        Debug.Log("Quit");
-        Application.Quit();
+        optionsMenu.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+
+        EventSystem.current.SetSelectedGameObject(optionsClosedButton);
+
+
+    }
+
+    public void OpenCredits()
+    {
+        creditsMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+
+        EventSystem.current.SetSelectedGameObject(creditsFirstButton);
+    }
+
+    public void CloseCredits()
+    {
+        creditsMenu.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+
+        EventSystem.current.SetSelectedGameObject(creditsBackButton);
     }
 }
